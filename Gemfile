2d08a639c65828435e6827b907d700dd51d49486
Prathamesh Sonpatki <csonpatki@gmail.com>
2011-09-07 02:20:12 +0530

    User model with passwords, and user show page

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ group :test do
   gem 'rspec', '2.6.0'
   gem 'webrat', '0.7.1'
   gem 'spork', '0.8.4'
+  gem 'factory_girl_rails', '1.0'
 end
 
 # Gems used only for assets and not required
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110906175521) do
+ActiveRecord::Schema.define(:version => 20110906203626) do
 
   create_table "companies", :force => true do |t|
     t.integer  "c_id"
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(:version => 20110906175521) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
