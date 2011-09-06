39612a4f49f80726a76858d070c1353f487c9011
Prathamesh Sonpatki <csonpatki@gmail.com>
2011-09-06 23:34:21 +0530

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Forum::Application.routes.draw do
+resources :users
+  
+  
+  
+
 
   get "users/new"
+match '/signup', :to => 'users#new'
+
 match '/signup', :to => 'users#new'
 match '/', :to => 'pages#home'
 match '/contact', :to => 'pages#contact'
